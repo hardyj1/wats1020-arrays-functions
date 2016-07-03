2432bad5734241ebba7a6445ead10291ae4e3904
hardyj1 <hardyj1@seattleu.edu>
2016-07-03 19:52:52 -0400

    truncated text

--- truncate.js
+++ truncate.js
@@ -1,23 +1,33 @@
 // This is the file where you will write the Truncate Words function and related code.
 
+var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
+var wordLimit = 8;
+
 // TODO: Create a Function called truncateWords() that accepts two arguments: 
 // longText (a String with several words in it)
 // numWords (an Integer that sets the number of words you want in the returned text)
 
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
+var truncateWords = function(longText, numWords){
+    // 1. Use the split() function to split the String into an Array
+    var longTextArray = originalText.split(' ');
+    // 2. Use the length attribute to find the number of words in the Array
+    numWords = originalText.length;
+  
+    // 3. Determine how many words should be removed from the String
+    // 4. Remove those words from the Array    
+    var shortTextArray = longTextArray.splice(0, 8);
+    // 5. Add an additional String item to the Array to put an ellipses in: "..."
+    shortTextArray.push('...');
+    // 6. Use the join() function to convert the Array back into a String
+    var shortText = shortTextArray.join (' ');
+    // 7. Return the truncated String from the Function
+    return shortText;
+}
 
 // This portion of the script is meant to call and display the result of your Function.
 // You do not need to change the following lines, but you may change them if you 
 // are experimenting further or pursuing stretch goals.
-var originalText = "There are two hard things in Computer Science: Cach invalidation, naming things, and off-by-one errors.";
-var wordLimit = 8;
+
 var shortText = truncateWords(originalText, wordLimit);
 console.log('originalText: ' + originalText);
 console.log('shortText: ' + shortText);
